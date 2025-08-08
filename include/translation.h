beccbb31d3e5552a6c7dc4753f8b2197e518d0a3
StrawFox64 <122846274+StrawFox64@users.noreply.github.com>
2025-08-08 05:14:41 -0300

    Moved translations to a separate file

---
+++ include/translation.h
@@ -0,0 +1,19 @@
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
+
+#include "locale.h"
+
+extern LocalisationMessages_t english;
+extern LocalisationMessages_t spanish;
+extern LocalisationMessages_t canadian_french;
+extern LocalisationMessages_t polish;
+extern LocalisationMessages_t brazilian_portuguese;
+extern LocalisationMessages_t portuguese;
+extern LocalisationMessages_t german;
+extern LocalisationMessages_t russian;
+extern LocalisationMessages_t korean;
+extern LocalisationMessages_t chinese_simplified;
+extern LocalisationMessages_t swedish;
+extern LocalisationMessages_t italian;
+
+#endif
